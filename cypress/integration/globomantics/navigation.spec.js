43f68d2826030383f7dd9dc76faf65f0f8aff8a0
Adhithi Ravichandran <adhithi@redivus.com>
2022-04-19 14:08:50 -0500

    Remove cypress folder for initial commit

--- cypress/integration/globomantics/navigation.spec.js
+++
@@ -1,9 +0,0 @@
-/// <reference types="cypress" />
-
-context("Navigation", () => {
-  it("should navigate to sessions page", () => {
-    cy.visit("/conference");
-
-    cy.get("h1").contains("View Sessions");
-  });
-});
