bcc01ad91f2fa521b3a38441316053ada1bd2754
Adhithi Ravichandran <adhithi@redivus.com>
2022-05-24 14:06:07 -0500

    Module 4 - Cypress Core concepts code

--- app/src/pages/conference/Sessions.jsx
+++ app/src/pages/conference/Sessions.jsx
@@ -139,6 +139,7 @@ export function Sessions() {
               type="button"
               onClick={() => setDay("All")}
               className="btn-oval"
+              data-cy="AllSessions"
             >
               All Sessions
             </button>
@@ -146,6 +147,7 @@ export function Sessions() {
               type="button"
               onClick={() => setDay("Wednesday")}
               className="btn-oval"
+              data-cy="Wednesday"
             >
               Wednesday
             </button>
@@ -153,6 +155,7 @@ export function Sessions() {
               type="button"
               onClick={() => setDay("Thursday")}
               className="btn-oval"
+              data-cy="Thursday"
             >
               Thursday
             </button>
@@ -160,6 +163,7 @@ export function Sessions() {
               type="button"
               onClick={() => setDay("Friday")}
               className="btn-oval"
+              data-cy="Friday"
             >
               Friday
             </button>
---
+++ cypress/integration/sessions.spec.js
@@ -0,0 +1,58 @@
+/// <reference types="cypress" />
+
+describe("Sessions page", () => {
+  // Run before each test in this describe block
+  beforeEach(() => {
+    cy.visit("/conference");
+    cy.get("h1").contains("View Sessions").click();
+    cy.url().should("include", "/sessions");
+
+    // Define aliases here
+    cy.get("[data-cy=AllSessions]").as("AllSessionsBtn");
+    cy.get("[data-cy=Wednesday]").as("WednesdayBtn");
+    cy.get("[data-cy=Thursday]").as("ThursdayBtn");
+    cy.get("[data-cy=Friday]").as("FridayBtn");
+  });
+
+  it("should navigate to conference sessions page and view day filter buttons", () => {
+    // Validate that buttons to filter by day exists
+    cy.get("@AllSessionsBtn");
+    cy.get("@WednesdayBtn");
+    cy.get("@ThursdayBtn");
+    cy.get("@FridayBtn");
+  });
+
+  it("should filter sessions and ony display Wednesday sessions when Wednesday button is clicked", () => {
+    cy.get("@WednesdayBtn").click();
+
+    // Assertion
+    cy.get("[data-cy=day]").should("have.length", 24);
+    cy.get("[data-cy=day]").contains("Wednesday").should("be.visible");
+    cy.get("[data-cy=day]").contains("Thursday").should("not.exist");
+    cy.get("[data-cy=day]").contains("Friday").should("not.exist");
+  });
+
+  it("should filter sessions and ony display Thursday sessions when Thursday button is clicked", () => {
+    cy.get("@ThursdayBtn").click();
+
+    cy.get("[data-cy=day]").contains("Wednesday").should("not.exist");
+    cy.get("[data-cy=day]").contains("Thursday").should("be.visible");
+    cy.get("[data-cy=day]").contains("Friday").should("not.exist");
+  });
+
+  it("should filter sessions and ony display Friday sessions when Friday button is clicked", () => {
+    cy.get("@FridayBtn").click();
+
+    cy.get("[data-cy=day]").contains("Wednesday").should("not.exist");
+    cy.get("[data-cy=day]").contains("Thursday").should("not.exist");
+    cy.get("[data-cy=day]").contains("Friday").should("be.visible");
+  });
+
+  it("should filter sessions and display all sessions when All Sessions button is clicked", () => {
+    cy.get("@AllSessionsBtn").click();
+
+    cy.get("[data-cy=day]").contains("Wednesday").should("be.visible");
+    cy.get("[data-cy=day]").contains("Thursday").should("be.visible");
+    cy.get("[data-cy=day]").contains("Friday").should("be.visible");
+  });
+});
